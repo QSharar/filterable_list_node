6b7bf9407a23f4c425e314bd368f58de70bd4abe
Quazi Sharar <quazisharar@Quazis-MacBook-Air.local>
2017-10-06 16:05:39 -0400

    First Commit

---
+++ app.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var bp = require("body-parser");
+var path = require("path");
+var exphbs = require("express-handlebars");
+
+
+var app = express();
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+app.use("/public" , express.static(path.join(__dirname, "public")));
+
+var names = [   "Andres" , 
+                "Beck" , 
+                "Cassie", 
+                "David" , 
+                "Sheri",
+                "Estell", 
+                "Lafortune",
+                "Allene", 
+                "Lemoine",
+                "Lorrie",
+                "Cissell",
+                "Karlyn",
+                "Clendening",
+                "Melda",
+                "Batchelder",
+                "Kourtney" ,
+                "Justin",
+                "Eveline",
+                "Matter",
+                "Gaye",
+                "Kaczynski"
+            ] 
+
+
+
+app.use(bp.urlencoded({extended: false}));
+app.use(bp.json());
+app.get("/page", (req,res) => res.render("page"));
+app.get("/", (req, res ) => {
+    console.log(`${req.method} -> ${req.url}`);
+    res.render('list', {msg: names});
+})
+
+app.listen(9000);
+
+
